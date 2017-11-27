f1b4b43a208158c0083d9913c65c956a1d86e7ff
dongwonkay <dongwon@likelion.org>
2017-11-27 05:24:58 +0000

    Fix video_uploader/Video for mount_uploaders

--- app/models/videopost.rb
+++ app/models/videopost.rb
@@ -1,6 +1,34 @@
+class VideoUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  include CarrierWave::Video
+
+  process encode_video: [:mp4, callbacks: { after_transcode: :set_success } ]
+  
+  def store_dir
+    "uploads/"
+  end
+    
+    
+end
+
 class Videopost < ActiveRecord::Base
+    mount_uploaders :file, VideoUploader
+
     validates :video_title, presence: { message: "제목써라 임마" }
     belongs_to :user
     has_many :videocomments
     
+    def set_success(format, opts)
+        self.success = true
+    end
 end
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,33 +1,3 @@
 # encoding: utf-8
 
-class VideoUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  include CarrierWave::Video
-
-  process encode_video: [:mp4, callbacks: { after_transcode: :set_success } ]
-  
-  def store_dir
-    "uploads/"
-  end
-    
-    
-end
-
-class Video
-  mount_uploader :file, VideoUploader
-
-  def set_success(format, opts)
-    self.success = true
-  end
-
-end

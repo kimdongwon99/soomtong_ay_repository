d584ccc16580f8ab0320c84ca0de900cc9f502a7
dongwonkay <dongwon@likelion.org>
2017-12-07 01:47:16 +0000

    aws_changeing..

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     region:                'ap-northeast-2',             # 아시아-한국서버 명칭
     endpoint:              'https://s3-ap-northeast-2.amazonaws.com' # 아시아-한국서버
   }
-  config.fog_directory  = 'likelion5'                          # required
+  config.fog_directory  = 'likelionhiphop'                          # required
   config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { } # optional, defaults to {}
 end
